122088eccfd21e840832b89a589920ef35e06da5
Michael Correale <Mcorreale1@gmail.com>
2015-04-13 13:43:40 -0400

    moved css reader

--- proctest.rb
+++ proctest.rb
@@ -17,11 +17,30 @@ def what_am_i(&block)
   block.class
 end
 
-ary1 = [1, 2, 3, 4]
-ary2 = [5, 6, 7, 8]
-ary3 = [9, 10, 11 ,12]
+DNA = Struct.new(:chain) do
+  def triples
+    chain.split(//).each_slice(3).to_a
+  end
 
-ary1.each_with_index{ |c, n|
-	ary2.insert(n, c)
-}
-puts ary2.inspect
+  def &(dna)
+  	puts triples.inspect
+  	puts dna.triples.inspect
+    (triples & dna.triples).map{ |triple| triple.join }
+  end
+end
+
+
+test = DNA.new('AGGTTACCA') & DNA.new('TTAAGGCCC')
+puts test.inspect
+
+
+
+
+# ary1 = [1, 2, 3, 4]
+# ary2 = [5, 6, 7, 8]
+# ary3 = [9, 10, 11 ,12]
+
+# ary1.each_with_index{ |c, n|
+# 	ary2.insert(n, c)
+# }
+# puts ary2.inspect
--- timeTest.rb
+++ timeTest.rb
@@ -1,14 +1,22 @@
-def do_interval(secs)
-	last_tick = Time.now
-	loop do
-		sleep 0.1
-		if Time.now - last_tick >= secs
-			last_tick += secs
-			yield
-		end
-	end
-end
+# def do_interval(secs)
+# 	last_tick = Time.now
+# 	loop do
+# 		sleep 0.1
+# 		if Time.now - last_tick >= secs
+# 			last_tick += secs
+# 			yield
+# 		end
+# 	end
+# end
 
-do_interval(1) do
-	puts Time.now
-end
+# do_interval(1) do
+# 	puts Time.now
+# end
+
+ary = []
+ary[0] = 1
+ary << 2
+hash = {}
+hash['test'] = "test"
+hash.merge!({"array" => ary})
+puts hash.inspect
